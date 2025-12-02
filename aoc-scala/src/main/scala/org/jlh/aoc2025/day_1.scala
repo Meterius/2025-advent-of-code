d2882b938901c17870bd07fda7b979e0ba84283d
Meterius <Meterius@users.noreply.github.com>
2025-12-02 15:56:14 +0100

    Add day 1 scala solution

--- aoc-scala/src/main/scala/org/jlh/aoc2025/day_1.scala
+++ aoc-scala/src/main/scala/org/jlh/aoc2025/day_1.scala
@@ -1,7 +1,45 @@
 package org.jlh.aoc2025
 
-def solve_day1_part1(data: Iterator[String]): Int =
-  return 0
+private def parseRotation(rotStr: String): Int = {
+  val dir = rotStr(0) match
+    case 'L' => -1
+    case 'R' => 1
+    case _ => throw new Error(s"Invalid rotation: $rotStr")
 
-def solve_day1_part2(data: Iterator[String]): Int =
-  return 0
+  dir * rotStr.substring(1).toInt
+}
+
+def solveDay1Part1(data: Iterator[String]): Int =
+  val maxPos = 100
+
+  var pos = 50
+  var counter = 0
+
+  for rotStr <- data do {
+    pos = scala.math.floorMod(pos + parseRotation(rotStr), maxPos)
+    if pos == 0 then
+      counter += 1
+  }
+
+  counter
+
+def solveDay1Part2(data: Iterator[String]): Int =
+  val maxPos = 100
+
+  var pos = 50
+  var counter = 0
+
+  for rotStr <- data do {
+    val rot = parseRotation(rotStr)
+    val rotK = rot / maxPos
+    val rotR = rot % maxPos
+
+    counter += rotK.abs
+
+    if pos != 0 then
+      counter += scala.math.floorDiv(pos - 1 + rotR, maxPos - 1).abs
+
+    pos = scala.math.floorMod(pos + rotR, maxPos)
+  }
+
+  counter
--- aoc-scala/src/main/scala/org/jlh/aoc2025/main.scala
+++ aoc-scala/src/main/scala/org/jlh/aoc2025/main.scala
@@ -5,18 +5,20 @@ def run(args: String*): Unit = {
   val day = args.lift(0).map(_.toInt)
 
   val solvers = Array(
-    (solve_day1_part1, solve_day1_part2)
+    (solveDay1Part1, solveDay1Part2)
   );
 
-  for ((solve_part1, solve_part2), idx) <- solvers.view.zipWithIndex do {
+  for ((solvePart1, solvePart2), idx) <- solvers.view.zipWithIndex do {
     val day_idx = idx + 1
     if day.fold(true)(day_idx == _) then
       println(s"Day $day_idx:")
       
       val data = scala.io.Source.fromResource(s"day_${day_idx}_data.txt").getLines()
-      val part1 = solve_part1(data)
+      val part1 = solvePart1(data)
       println(s"\tPart 1: $part1")
-      val part2 = solve_part2(data)
+
+      val data2 = scala.io.Source.fromResource(s"day_${day_idx}_data.txt").getLines()
+      val part2 = solvePart2(data2)
       println(s"\tPart 2: $part2")
   }
 }
